bff376cfb8e40297a15ec40e6fae791dd0ce1426
DirtyMetis <dirtymetis@hotmail.com>
2019-12-02 18:44:36 -0600

    Updated input2.txt

--- day_01/day_01.js
+++ day_01/day_01.js
@@ -18,7 +18,7 @@ const calcFuelTest = (tests) => {
 console.log(calcFuelTest(testArray));
 
 //PART ONE
-const modulesString =
+const modulesString1 =
 "140170, 75120, 75645, 134664, 124948, 137630, 146662, 116881, 120030, 94332, " +
 "50473, 59361, 128237, 84894, 51368, 128802, 57275, 129235, 113481, 66378, " +
 "55842, 90548, 107696, 53603, 130458, 80306, 120820, 131313, 100303, 59224, " +
@@ -30,14 +30,15 @@ const modulesString =
 "104609, 149559, 136037, 54997, 139674, 101638, 65739, 70029, 143847, 122035, " +
 "66256, 78087, 105045, 108867, 99630, 127173, 139021, 139759, 134171, 104869";
 
-const modulesArray = modulesString.split(", ").map(string => parseInt(string));
+const modulesArray1 = modulesString1.split(", ").map(string => parseInt(string));
 
 const modulesString2 =
 "140170 75120 75645 134664 124948 137630 146662 116881 120030 94332 50473 59361 128237 84894 51368 128802 57275 129235 113481 66378 55842 90548 107696 53603 130458 80306 120820 131313 100303 59224 123369 140584 60642 68184 103101 82278 51968 51048 98139 60498 127082 71197 109478 71286 84840 141305 51800 72352 93147 73549 122739 62363 58453 59000 63564 63424 51053 120826 123337 130824 59053 77983 68977 67126 96051 53024 145647 139343 113236 59396 146174 148622 83384 86938 100673 80757 107675 147417 124538 136463 104609 149559 136037 54997 139674 101638 65739 70029 143847 122035 66256 78087 105045 108867 99630 127173 139021 139759 134171 104869";
 
-const modulesArray2 = modulesString.split(" ").map(string => parseInt(string));
+const modulesArray2 = modulesString2.split(" ").map(string => parseInt(string));
 
-const masses = fs.readFileSync('input.txt','utf8').split('\n');
+const masses1 = fs.readFileSync('input1.txt','utf8').split('\n');
+const masses2 = fs.readFileSync('input2.txt','utf8').split('\n');
 
 const calcFuel = (modules) => {
   var sum = 0;
@@ -50,8 +51,10 @@ const calcFuel = (modules) => {
   return sum;
 };
 
-console.log(calcFuel(modulesArray));
+console.log(calcFuel(modulesArray1));
 
 console.log(calcFuel(modulesArray2));
 
-console.log(calcFuel(masses));
+console.log(calcFuel(masses1));
+
+console.log(calcFuel(masses2));
